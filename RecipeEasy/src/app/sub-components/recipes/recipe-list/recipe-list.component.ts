a316c18977b7669146771542a1b7ca80b45a5084
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-04-23 18:52:07 -0300

    Passing ingredients from recipes to shopping list

--- RecipeEasy/src/app/sub-components/recipes/recipe-list/recipe-list.component.ts
+++ RecipeEasy/src/app/sub-components/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,8 +10,8 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit{
   recipes: Recipe[] = [
-    new Recipe("Strogotest", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg"),
-    new Recipe("AnotherRecipe", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg")
+    new Recipe("Strogotest", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg", [new Ingredient("Bread", 3), new Ingredient("Peper", 1)]),
+    new Recipe("AnotherRecipe", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg", [new Ingredient("Bread", 3), new Ingredient("Peper", 1)])
   ];
 
   constructor(private recipeService: RecipeService ){}
--- RecipeEasy/src/app/sub-components/recipes/recipe.service.ts
+++ RecipeEasy/src/app/sub-components/recipes/recipe.service.ts
@@ -1,15 +1,24 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "src/app/shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe("Strogotest", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg"),
-        new Recipe("AnotherRecipe", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg")
+        new Recipe("Strogotest", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg", [new Ingredient("Meat", 2), new Ingredient("Salt", 10)]),
+        new Recipe("AnotherRecipe", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg", [new Ingredient("Bread", 3), new Ingredient("Peper", 1)])
     ];
+
+    constructor(private slService: ShoppingListService){}
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
