63a4852c9ec8bb3a74a69ef5e32b338eff1978f7
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-04-25 16:34:12 -0300

    Programmatic navigation to edit recipe page

--- RecipeEasy/src/app/sub-components/recipes/recipe-list/recipe-list.component.ts
+++ RecipeEasy/src/app/sub-components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
@@ -14,10 +15,14 @@ export class RecipeListComponent implements OnInit{
     new Recipe("AnotherRecipe", "lorem ipsum description lorem ipsum", "https://m.media-amazon.com/images/I/615AR5oTwsL.jpg", [new Ingredient("Bread", 3), new Ingredient("Peper", 1)])
   ];
 
-  constructor(private recipeService: RecipeService ){}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute){}
 
   ngOnInit(): void {
       this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
